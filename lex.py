a704f4a4bd609dfe776f920693bcba342ed0de11
chockleyc <chockleyc@gmail.com>
2013-11-12 11:12:47 -0600

    Fixed lex.py to have appropriate indentation.

--- lex.py
+++ lex.py
@@ -1,76 +1,73 @@
 class lexer:
-  def __init__(self, filename):
-    self.f = open(file, 'r')
-    self.ch = ''
+    def __init__(self, filename):
+        self.f = open(file, 'r')
+        self.ch = ''
 
-  class lexeme:
-    def __init__(self, aType = None, aData = None):
-      self.type = aType
-      self.data = aData
-   
-  def lex():
-    skipWhiteSpace()
+    class lexeme:
+        def __init__(self, aType = None, aData = None):
+            self.type = aType
+            self.data = aData
 
-    self.ch = f.read(1)
-  
-    if len(ch) < 1:
-      return lexeme(ENDofINPUT)
-  
-    if ch == '(':
-      return lexeme(OPAREN) 
-  
-    elif ch == ')':
-      return lexeme(CPAREN) 
-  
-    elif ch == ',':
-      return lexeme(COMMA) 
-  
-    elif ch == '+':
-      return lexeme(PLUS) 
-  
-    elif ch == '*':
-      return lexeme(TIMES) 
-  
-    elif ch == '/':
-      return lexeme(DIVIDES) 
-  
-    elif ch == '<':
-      return lexeme(LESSTHAN) 
-  
-    elif ch == '>':
-      return lexeme(GREATERTHAN) 
-  
-    elif ch == '=':
-      return lexeme(ASSIGN) 
-  
-    elif ch == ';':
-      return lexeme(SEMICOLON) 
-  
-    else:
-      if isDigit(ch):
-        pushBack(ch)
-        return lexNumber()
-  
-      elif isLetter(ch):
-        pushBack(ch)
-        return lexVariable()
-  
-      elif ch == '\"':
-        pushBack(ch)
-        return lexString()
-  
-      else:
-        return lexeme(UNKNOWN, ch)
-  
-  def skipWhiteSpace():
-    while self.ch.isspace():
-      self.ch = f.read(1)
-  
-  
-  def lexNumber():
-  
-  
-  def lexVaribale():
+    def lex():
+        skipWhiteSpace()
 
-  
-  def lexString():
+        self.ch = f.read(1)
+
+        if len(self.ch) < 1:
+            return lexeme(ENDofINPUT)
+
+        elif self.ch == '(':
+            return lexeme(OPAREN)
+
+        elif self.ch == ')':
+            return lexeme(CPAREN)
+
+        elif self.ch == ',':
+            return lexeme(COMMA)
+
+        elif self.ch == '+':
+            return lexeme(PLUS)
+
+        elif self.ch == '*':
+            return lexeme(TIMES)
+
+        elif self.ch == '/':
+            return lexeme(DIVIDES)
+
+        elif self.ch == '<':
+            return lexeme(LESSTHAN)
+
+        elif self.ch == '>':
+            return lexeme(GREATERTHAN)
+
+        elif self.ch == '=':
+            return lexeme(ASSIGN)
+
+        elif self.ch == ';':
+            return lexeme(SEMICOLON)
+
+        else:
+            if isDigit(self.ch):
+                pushBack(self.ch)
+                return lexNumber()
+
+            elif isLetter(self.ch):
+                pushBack(self.ch)
+                return lexVariable()
+
+            elif self.ch == '\"':
+                pushBack(self.ch)
+                return lexString()
+
+            else:
+                return lexeme(UNKNOWN, self.ch)
+
+    def skipWhiteSpace():
+        while self.ch.isspace():
+            self.ch = f.read(1)
+
+    def lexNumber():
+
+    def lexVariable():
+
+    def lexString():
